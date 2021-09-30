d641e4412e8df4f38cbeea758ee37c1fee71c913
beginor <beginor@qq.com>
2021-09-30 11:43:13 +0800

    enable hash, add terrain.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,7 @@
+import mapboxgl from 'mapbox-gl';
 // import { I3SLoader } from '@loaders.gl/i3s';
 // import { Tile3DLayer } from '@deck.gl/geo-layers';
 // import { MapboxLayer } from '@deck.gl/mapbox';
-
-import mapboxgl from 'mapbox-gl';
 
 import { accessToken, defaultStyleUrl } from '../config';
 
@@ -28,9 +27,26 @@ export class App {
             center: [113.087100, 23.655254 ],
             zoom: 6.32,
             pitch: 0,
-            bearing: 0
+            bearing: 0,
+            hash: true
         });
         this.map = map;
+        map.on('load', () => {
+            this.addTerrain();
+        });
+    }
+
+    private addTerrain(): void {
+        this.map.addSource(
+            'mapbox-terrain',
+            {
+                type: 'raster-dem',
+                url: 'mapbox://mapbox.mapbox-terrain-dem-v1',
+                tileSize: 512,
+                maxzoom: 14
+            }
+        );
+        this.map.setTerrain({ source: 'mapbox-terrain', exaggeration: 1.5 });
     }
 
 }
